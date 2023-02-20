c6106ca211d3889877ad9cb56da3d2c9cc4d5a2f
Matt Wyatt <matt.wyatt@veteransunited.com>
2023-02-20 15:08:27 -0600

    2

--- compoundInterest.py
+++ compoundInterest.py
@@ -2,10 +2,23 @@ def calculateCompoundInterest():
     
 # This first 3 lines are provided for yougetACompoundInterest()
 # This first 3 lines are provided for you
- client_one_principal = float(input("Principle (amount): "))
- client_one_time =      float(input("Time:               "))
- client_one_rate =      float(input("Rate:               "))
+    client_one_principal = float(input("Principle (amount): "))
+    client_one_time =      float(input("Time:               "))
+    client_one_rate =      float(input("Rate:               "))
  #print("Compound Interest: "+str(intrest))
+
+
+    total = round(client_one_principal*(1+client_one_rate / 100)**client_one_time, 2)
+    interest = round((total) - client_one_principal, 2)
+ 
+    print(f"Compound Interest:  {interest}")
+    #print("---")
+    
+
+
+
+
+
 
     # end assignment
 
